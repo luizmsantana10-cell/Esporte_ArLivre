5458d7202d8b7218bd519f5581715e75b18ea8d0
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-08-13 14:09:59 -0300

    pasta component criada

---
+++ src/app/component/menu-component/menu-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-component.component.html',
+  styleUrl: './menu-component.component.css'
+})
+export class MenuComponentComponent {
+
+}
