667e9a9b50789ef702f9a460c9e678f6884edef6
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-08-13 16:42:50 -0300

    sim

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { Routes } from '@angular/router';
 
-import { MenuComponentComponent } from './component/menu-component/menu-component.component';
+
 import { AtletaComponent } from './component/atleta/atleta.component';
 import { ComponentFixture } from '@angular/core/testing';
 import { HomeComponentComponent } from './component/home-component/home-component.component';
 
 export const routes: Routes = [
-
-   
-
-    
-     {
+    {
         path: '',
-        component: MenuComponentComponent
+        redirectTo:"/home",
+        pathMatch:"full"
+    
+    },
+
+    {
+        path: 'home',
+        component: HomeComponentComponent
+    
     },
     {
         path: 'atleta',
         component: AtletaComponent
-    },
-    {
-        path: '',
-        component: HomeComponentComponent
-    
     }
+   
 ];
