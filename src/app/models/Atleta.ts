472be1f5e647b13a7fe3744c551a4417159d02f4
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-08-14 14:07:11 -0300

    a

---
+++ src/app/models/Atleta.ts
@@ -0,0 +1,10 @@
+export class Atleta{
+    id: number = 0
+    nome: string = ''
+    cpf: number = 0
+    sexo: string = ''
+    cep: number = 0
+    ruaLogradouro: string = ''
+    bairro: string = ''
+    cidade: string= ''
+}
